47e6d7c8870be405ce4454b4c1d22fa6fdcef76f
washingtonmlourenco <washingtonmagelalourenco@gmail.com>
2023-09-18 16:25:34 -0300

    #ReadErrorlog.sql

---
+++ ReadErrorlog.sql
@@ -0,0 +1,21 @@
+IF EXISTS (
+		SELECT *
+		FROM sys.tables
+		WHERE [name] LIKE 'tempdb..#ReadErrorlog'
+		)
+	DROP TABLE #ReadErrorlog
+GO
+
+CREATE TABLE #ReadErrorlog (
+	 LogDate DATETIME
+	,ProcessInfo VARCHAR(100)
+	,TEXT VARCHAR(MAX)
+	)
+GO
+
+INSERT INTO #ReadErrorlog
+EXEC sp_readerrorlog;
+
+SELECT *
+FROM #ReadErrorlog
+WHERE ProcessInfo = 'Backup'
