cd65620c79a89b1af0342419b3f812dde7234455
Washington M. Lourenço |  AZ-900 <67602627+washingtonmlourenco@users.noreply.github.com>
2023-09-19 01:17:24 -0300

    Update ReadErrorlog.sql

--- ReadErrorlog.sql
+++ ReadErrorlog.sql
@@ -1,4 +1,4 @@
--- Developed by my friend Kelson Damasceno
+-- Developed by my friend Kelson Damasceno https://www.linkedin.com/in/ACoAAB79tb4B5IbXIwrhGRqaCqeIBrizBMYlGdI?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recent_activity_content_view%3Bs9nC%2BP7ATiC2io8M0xqZVg%3D%3D
 IF EXISTS (
 		SELECT *
 		FROM sys.tables
