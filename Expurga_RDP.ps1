1c2b41200ad4fc41ca9fbab8f944da125052e7ca
washingtonmlourenco <washingtonmagelalourenco@gmail.com>
2023-09-19 00:37:39 -0300

    #Remove .RDP File of Directory

---
+++ Expurga_RDP.ps1
@@ -0,0 +1,14 @@
+﻿# Seta o diretório de atual para o diretório de downloads!
+Set-Location 'C:\Users\washington.lourenco\Downloads'
+
+# Lista todos os arquivos .RDP no diretório de downloads e, subpastas!
+Get-ChildItem -Recurse -File -Filter *.rdp
+
+# Remove todos os arquivos com extensão .RDP da pasta de Downloads
+Remove-Item -Path *.rdp
+
+
+
+
+
+
